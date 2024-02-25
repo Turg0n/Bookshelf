f06e39da1ffebec1b45784d92aed1909d914d7d6
Turg0n <turgon1661@gmail.com>
2024-02-25 20:34:25 +0200

    sort

--- src/main.js
+++ src/main.js
@@ -2,7 +2,15 @@ import './js/categories';
 import './js/shoppingListService';
 import './js/modal';
 import mobMenu from './js/mobMenu/mobMenu';
-
+import './js/addBooksListeners';
+import './js/apiService';
+import './js/createBookCard';
+import './js/homeRefs';
+import './js/mobMenu';
+import './js/pagination';
+import './js/renderingByCategory';
+import './js/renderingHomePage';
+import './js/spin';
 window.addEventListener('DOMContentLoaded', () => {
   mobMenu();
 });
