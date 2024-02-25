e0b4dcec7e3a24fca95022890443e6f946a706d4
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-02-25 19:06:05 +0200

    css_2

--- src/partials/js/pagination.js
+++ src/partials/js/pagination.js
@@ -11,15 +11,15 @@ let paginationOptions   =   {
         page: '<a href="#" class="tui-page-btn">{{page}}</a>',
         currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
         moveButton:
-          '<a href="#" class="tui-page-btn tui-page-btn-moveButton tui-{{type}}">' +
+          '<a href="#" class="tui-page-btn tui-page-btn-moveButton">' +
             '<span class="tui-ico-{{type}}">{{type}}</span>' +
           '</a>',
         disabledMoveButton:
-          '<span class="tui-page-btn tui-page-btn-disabledMoveButton tui-is-disabled tui-{{type}}">' +
+          '<span class="tui-page-btn tui-page-btn-disabledMoveButton tui-is-disabled">' +
             '<span class="tui-ico-{{type}}">{{type}}</span>' +
           '</span>',
         moreButton:
-          '<a href="#" class="tui-page-btn ui-page-btn-moreButton tui-{{type}}-is-ellip">' +
+          '<a href="#" class="tui-page-btn tui-{{type}}-is-ellip">' +
             '<span class="tui-ico-ellip">...</span>' +
           '</a>'
       }
