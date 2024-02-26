7526606b48f6c749e6cbfac969d8de254e611e5c
Turg0n <turgon1661@gmail.com>
2024-02-26 20:04:24 +0200

    Update categories.js

--- src/js/categories.js
+++ src/js/categories.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import getBooksByCategory from './serviceBooks';
+import {getBooksByCategory} from './serviceBooks';
 const category_list = document.querySelector('.nav-categories-list');
 
 async function getCategoryList() {
