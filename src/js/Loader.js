e0e29c08314e7d8b2158478e03830bf17a501f7d
y.trotskiy <143190183+TrotskyuYurii@users.noreply.github.com>
2024-02-27 23:21:28 +0200

    fix_loader

--- src/js/Loader.js
+++ src/js/Loader.js
@@ -1,7 +1,8 @@
 export function showLoader() {
   const loader = document.querySelector('#loader');
   if (loader) {
-    loader.classList.add('visible');
+    // loader.classList.add('visible');
+    loader.style.display = 'flex';
   } else {
     console.error('Loader element not found.');
   }
@@ -10,7 +11,8 @@ export function showLoader() {
 export function hideLoader() {
   const loader = document.querySelector('#loader');
   if (loader) {
-    loader.classList.remove('visible');
+    // loader.classList.remove('visible');
+    loader.style.display = 'none';
   } else {
     console.error('Loader element not found.');
   }
