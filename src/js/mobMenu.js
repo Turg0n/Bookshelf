f2a9d6121be651e9a755757af7eebb85c2a87941
RomanYurkiv <yurkiv.r.official@gmail.com>
2024-02-28 21:13:28 +0200

    stroke to close btn

--- src/js/mobMenu.js
+++ src/js/mobMenu.js
@@ -4,7 +4,7 @@ const burgerBtn = document.querySelector('.js-open-menu');
 
 burgerBtn.insertAdjacentHTML(
   'afterbegin',
-  `<svg class="icon-close visually-hidden" width="28" height="28">
+  `<svg class="icon-close visually-hidden " width="28" height="28">
     <use href="./img/sprite.svg#icon-x-close"></use>
   </svg>`
 );
